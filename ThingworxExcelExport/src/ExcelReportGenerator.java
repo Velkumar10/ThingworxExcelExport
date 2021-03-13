182ea0b332751c7ba6c84197abb5f581299758c9
Velkumar R <Velkumar R@VR>
2021-03-13 20:48:24 +0530

    Initial Commit

---
+++ ThingworxExcelExport/src/ExcelReportGenerator.java
@@ -0,0 +1,9 @@
+import com.thingworx.resources.Resource;
+
+public class ExcelReportGenerator extends Resource {
+
+	public ExcelReportGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
